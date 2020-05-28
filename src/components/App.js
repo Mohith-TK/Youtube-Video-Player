3fe0d6da425745eb11ff82ac96db78b038c37517
Colossal-Cyclone <mohithtk1707@gmail.com>
2020-05-28 14:03:35 +0530

    v1

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VidoeList";
+import VideoDetail from "./VideoDetail";
+const KEY = "API_KEY";
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+        key: `${KEY}`,
+        q: term,
+      },
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  componentDidMount() {
+    this.onTermSubmit("cars");
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
